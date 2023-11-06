6cd748715b38c0c808cfc8e4484a276290918a8c
Donatello Fantauzzi <donatello.fantauzzi@gmail.com>
2023-11-06 12:29:31 +0100

    create file Gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+gulp.task("nunjucks", function () {
+  // Gets .html and .nunjucks files in pages
+  return (
+    gulp
+      .src("app/src/pages/**/*.+(html|njk)")
+      .pipe(
+        data(function () {
+          return require("./app/data.json");
+        })
+      )
+      // Renders template with nunjucks
+      .pipe(
+        nunjucksRender({
+          path: ["app/src/"],
+        })
+      )
+      // output files in app folder
+      .pipe(gulp.dest("app/dist"))
+  );
+});
